06d3ed149506aff4982b0f97caf45e5fa9e2a22a
Ziad Tamer <ziadtamer0541@gmail.com>
2025-03-22 03:46:31 +0200

    v6

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://bjdudmwyelzvscaafaok.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqZHVkbXd5ZWx6dnNjYWFmYW9rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzE1NDk3NSwiZXhwIjoyMDQ4NzMwOTc1fQ.9iXG8eP-KEAo_-tNF_ysjW_DuahqdhKd_1_X8IU0RNE";
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
