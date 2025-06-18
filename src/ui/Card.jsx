ab3e1f13e74082c989684a6081c34ab9be103119
Ziad Tamer <ziadtamer0543@gmail.com>
2025-06-18 20:39:58 +0300

    Add Update Plan

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function Card({ children }) {
   return (
-    <div className="p-5 transition-all bg-white border border-gray-100 rounded-lg shadow-sm">
+    <div className="p-5 transition-all bg-white border border-gray-200 rounded-lg shadow-sm">
       {children}
     </div>
   );
